beefefcd3afd58c2a8e249f7040eb1da0f2e82c6
Santiago Brenes <itssanti_@outlook.com>
2025-04-09 18:40:23 -0600

    Initial commit

---
+++ src/main/java/com/example/libreriasanti/LibreriaSantiApplication.java
@@ -0,0 +1,13 @@
+package com.example.libreriasanti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaSantiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibreriaSantiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/libreriasanti/LibreriaSantiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libreriasanti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaSantiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
